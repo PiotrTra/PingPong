f55ec67140c2a73dcd17bff95cc5918163773d29
PiotrTra <ponczuszek363@gmail.com>
2023-11-27 01:26:03 +0100

    Creating a class called ball.py which create the object of the ball and make the ball move

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
+
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -9,6 +12,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -18,6 +22,8 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    ball.move()
     screen.update()
+    time.sleep(0.1)
 
 screen.exitonclick()
