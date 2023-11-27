aa78de6014f10a820b20b4ebe018d84daa90208a
PiotrTra <ponczuszek363@gmail.com>
2023-11-27 01:13:18 +0100

    Creating a class called paddle.py that create right and left and move to OOP

--- main.py
+++ main.py
@@ -1,33 +1,20 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
-paddle = Turtle()
-
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("PingPong")
 screen.tracer(0)
 
-paddle.shape("square")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
-paddle.color("white")
-
-
-def go_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def go_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
