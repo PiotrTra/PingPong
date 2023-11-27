feb3cf4639f652d0ba4a1231c22cb00fb294e937
PiotrTra <ponczuszek363@gmail.com>
2023-11-27 14:31:23 +0100

    Add the logic when right paddle and left paddle misses ball

--- main.py
+++ main.py
@@ -23,12 +23,19 @@ screen.onkey(l_paddle.go_down, "s")
 game_is_on = True
 while game_is_on:
     ball.move()
+
     # Detect collision with wall
     if ball.ycor() == 280 or ball.ycor() == -280:
         ball.bounce_y()
     # Detect collision with paddle
     if ball.distance(r_paddle) < 40 and ball.xcor() > 320 or ball.distance(l_paddle) < 40 and ball.xcor() < -320:
         ball.bounce_x()
+    # Detect r_paddle misses
+    if ball.xcor() > 400:
+        ball.reset_position()
+    # Detect l_paddle misses
+    if ball.xcor() < -400:
+        ball.reset_position()
 
     screen.update()
     time.sleep(0.1)
